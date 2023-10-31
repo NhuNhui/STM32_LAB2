83d6220d082e5e4e7809b12dd841b77d7e139125
NhuNhui <giaqui1403@gmail.com>
2023-10-31 23:19:03 +0700

    Exe9

---
+++ Core/Inc/Timer.h
@@ -0,0 +1,20 @@
+/*
+ * Timer.h
+ *
+ *  Created on: Oct 6, 2023
+ *      Author: QUI
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+extern int timer1_flag;
+extern int timer2_flag;
+
+void setTimer1(int duration);
+void timerRun1();
+
+void setTimer2(int duration);
+void timerRun2();
+
+#endif /* INC_TIMER_H_ */
